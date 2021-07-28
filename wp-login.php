a279576753a8def72c0f03d3a16400d25f3fbaa2
skillvire <87380481+skillvire@users.noreply.github.com>
2021-07-28 15:49:37 +0530

    changes

--- wp-login.php
+++ wp-login.php
@@ -87,7 +87,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/skillvire\/wp-admin\/admin-ajax.php"}};
 </script>
 <script src="https://skillvire.ga/wp-includes/js/wp-util.min.js?ver=5.8" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"99588316ac"};
+var userProfileL10n = {"user_id":"0","nonce":"fde7278d94"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
