22890c24543c59722f649ffce16aaeb2cfab7eb2
skillvire <skillvire@gmail.com>
2022-03-12 00:08:35 +0530

    domain change

--- wp-login.php
+++ wp-login.php
@@ -5,17 +5,17 @@
 	<title>Log In &lsaquo; SKILLVIRE &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="https://skillvire.ga/wp-includes/css/dashicons.min.css?ver=5.8.2" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://skillvire.ga/wp-includes/css/buttons.min.css?ver=5.8.2" media="all">
-<link rel="stylesheet" id="forms-css" href="https://skillvire.ga/wp-admin/css/forms.min.css?ver=5.8.2" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://skillvire.ga/wp-admin/css/l10n.min.css?ver=5.8.2" media="all">
-<link rel="stylesheet" id="login-css" href="https://skillvire.ga/wp-admin/css/login.min.css?ver=5.8.2" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://skillvire.gq/wp-includes/css/dashicons.min.css?ver=5.9.2" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://skillvire.gq/wp-includes/css/buttons.min.css?ver=5.9.2" media="all">
+<link rel="stylesheet" id="forms-css" href="https://skillvire.gq/wp-admin/css/forms.min.css?ver=5.9.2" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://skillvire.gq/wp-admin/css/l10n.min.css?ver=5.9.2" media="all">
+<link rel="stylesheet" id="login-css" href="https://skillvire.gq/wp-admin/css/login.min.css?ver=5.9.2" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
-	<link rel="icon" href="https://skillvire.ga/wp-content/uploads/2021/07/cropped-siteicon-32x32.png" sizes="32x32">
-<link rel="icon" href="https://skillvire.ga/wp-content/uploads/2021/07/cropped-siteicon-192x192.png" sizes="192x192">
-<link rel="apple-touch-icon" href="https://skillvire.ga/wp-content/uploads/2021/07/cropped-siteicon-180x180.png">
-<meta name="msapplication-TileImage" content="https://skillvire.ga/wp-content/uploads/2021/07/cropped-siteicon-270x270.png">
+	<link rel="icon" href="https://skillvire.gq/wp-content/uploads/2021/07/cropped-siteicon-32x32.png" sizes="32x32">
+<link rel="icon" href="https://skillvire.gq/wp-content/uploads/2021/07/cropped-siteicon-192x192.png" sizes="192x192">
+<link rel="apple-touch-icon" href="https://skillvire.gq/wp-content/uploads/2021/07/cropped-siteicon-180x180.png">
+<meta name="msapplication-TileImage" content="https://skillvire.gq/wp-content/uploads/2021/07/cropped-siteicon-270x270.png">
 	</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
 	<script type="text/javascript">
@@ -24,7 +24,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="https://skillvire.ga/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://skillvire.gq/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
@@ -42,31 +42,31 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
-									<input type="hidden" name="redirect_to" value="https://skillvire.ga/wp-admin/">
+									<input type="hidden" name="redirect_to" value="https://skillvire.gq/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="https://skillvire.ga/wp-login.php?action=lostpassword">Lost your password?</a>
+								<a href="https://skillvire.gq/wp-login.php?action=lostpassword">Lost your password?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="https://skillvire.ga/">&larr; Go to SKILLVIRE</a>		</p>
-		<div class="privacy-policy-page-link"><a class="privacy-policy-link" href="https://skillvire.ga/policies/">Policies</a></div>	</div>
-	<script src="https://skillvire.ga/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script src="https://skillvire.ga/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+			<a href="https://skillvire.gq/">&larr; Go to SKILLVIRE</a>		</p>
+		<div class="privacy-policy-page-link"><a class="privacy-policy-link" href="https://skillvire.gq/policies/">Policies</a></div>	</div>
+			<script src="https://skillvire.gq/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script src="https://skillvire.gq/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script id="zxcvbn-async-js-extra">
-var _zxcvbnSettings = {"src":"https:\/\/skillvire.ga\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/skillvire.gq\/wp-includes\/js\/zxcvbn.min.js"};
 </script>
-<script src="https://skillvire.ga/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script src="https://skillvire.ga/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7" id="regenerator-runtime-js"></script>
-<script src="https://skillvire.ga/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script src="https://skillvire.ga/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de" id="wp-hooks-js"></script>
-<script src="https://skillvire.ga/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79" id="wp-i18n-js"></script>
+<script src="https://skillvire.gq/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="https://skillvire.gq/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
+<script src="https://skillvire.gq/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script src="https://skillvire.gq/wp-includes/js/dist/hooks.min.js?ver=1e58c8c5a32b2e97491080c5b10dc71c" id="wp-hooks-js"></script>
+<script src="https://skillvire.gq/wp-includes/js/dist/i18n.min.js?ver=30fcecb428a0e8383d3776bcdd3a7834" id="wp-i18n-js"></script>
 <script id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -80,14 +80,14 @@ var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script src="https://skillvire.ga/wp-admin/js/password-strength-meter.min.js?ver=5.8.2" id="password-strength-meter-js"></script>
-<script src="https://skillvire.ga/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
+<script src="https://skillvire.gq/wp-admin/js/password-strength-meter.min.js?ver=5.9.2" id="password-strength-meter-js"></script>
+<script src="https://skillvire.gq/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
 <script id="wp-util-js-extra">
 var _wpUtilSettings = {"ajax":{"url":"\/skillvire\/wp-admin\/admin-ajax.php"}};
 </script>
-<script src="https://skillvire.ga/wp-includes/js/wp-util.min.js?ver=5.8.2" id="wp-util-js"></script>
+<script src="https://skillvire.gq/wp-includes/js/wp-util.min.js?ver=5.9.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"8c1e6328ec"};
+var userProfileL10n = {"user_id":"0","nonce":"e93ce7e369"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
@@ -96,7 +96,7 @@ var userProfileL10n = {"user_id":"0","nonce":"8c1e6328ec"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script src="https://skillvire.ga/wp-admin/js/user-profile.min.js?ver=5.8.2" id="user-profile-js"></script>
+<script src="https://skillvire.gq/wp-admin/js/user-profile.min.js?ver=5.9.2" id="user-profile-js"></script>
 			<script>
 			/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
 			</script>
